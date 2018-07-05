fcfb25cd761ee76f045fb50ff81462d3620b9761
paulwinex <paulwinex@gmail.com>
2018-07-05 19:22:32 +0300

    change job creation command

--- python/custom_af.py
+++ python/custom_af.py
@@ -1,5 +1,5 @@
 from pymel.all import *
-import os
+import os, getpass, socket, json
 
 
 if os.getenv('CGRU_LOCATION') and os.getenv('MAYA_CGRU_LOCATION'):
@@ -51,44 +51,98 @@ class meArnoldRenderVersion(meArnoldRender.meArnoldRender):
         self.generate_cmd_script()
 
     def generate_cmd_script(self):
-        # ass_dirname = self.ass_param['ass_dirname']
-        # path = cmds.workspace(expandName=ass_dirname)
         inp = self.get_assgen_options('defaultRenderLayer').replace('"', '') + self.job_param[
             'job_separator'] + '@{}@'.format('#' * self.job_param['job_padding']) + '.ass'
         inp = inp.replace('-filename', '').strip()
+
+        job_file = json_template.copy()
+        job_file['job']['blocks'][0]['frame_first'] = self.job_param['job_start']
+        job_file['job']['blocks'][0]['frame_last'] = self.job_param['job_end']
+        job_file['job']['blocks'][0]['frames_inc'] = self.job_param['job_step']
+        cmd = 'kick -i "{}" -v 2 -nstdin -dw -dp'.format(inp.replace('\\', '/'))
+        job_file['job']['blocks'][0]['command'] = cmd
+        job_file['job']['blocks'][0]['working_directory'] = os.path.dirname(inp)
+        json.dump(job_file, open(os.path.normpath(os.path.join(os.path.dirname(inp), 'render.sh')), 'w'), indent=2)
+
         CGRU_ROOT = get_cgru_root()
-        cmd_win = r'''set PYTHONPATH={CGRU_ROOT}\afanasy\python;{CGRU_ROOT}\lib\python;%PYTHONPATH%
+# {CGRU_ROOT}\python\python.exe "{CGRU_ROOT}\afanasy\python\afjob.py" "{INP}" {START} {END} -by {STEP} -fpt 1 -seq 1 -pwd "{PWD}" -name "{NAME}"  -pause
+        cmd_win = r'''
+set START_DIR=%~dp0
+set PYTHONPATH={CGRU_ROOT}\afanasy\python;{CGRU_ROOT}\lib\python;%PYTHONPATH%
 cd {CGRU_ROOT}
 call {CGRU_ROOT}\setup.cmd
-{CGRU_ROOT}\python\python.exe "{CGRU_ROOT}\afanasy\python\afjob.py" "{INP}" {START} {END} -by {STEP} -fpt 1 -seq 1 -pwd "{PWD}" -name "{NAME}"  -pause
+{CGRU_ROOT}\python\python.exe "{CGRU_ROOT}\afanasy\bin\afcmd.exe" json send "%START_DIR%\job.json"
         '''.format(
             CGRU_ROOT=CGRU_ROOT,
-            INP=inp,
-            START=self.job_param['job_start'],
-            END=self.job_param['job_end'],
-            STEP=self.job_param['job_step'],
-            PWD=os.path.dirname(inp),
-            NAME=os.path.basename(sceneName())
+            # INP=inp,
+            # START=self.job_param['job_start'],
+            # END=self.job_param['job_end'],
+            # STEP=self.job_param['job_step'],
+            # PWD=os.path.dirname(inp),
+            # NAME=os.path.basename(sceneName())
         )
         open(os.path.normpath(os.path.join(os.path.dirname(inp), 'render.cmd')), 'w').write(cmd_win)
 
         CGRU_ROOT = get_cgru_root()
-        cmd_bash = r'''export PYTHONPATH={CGRU_ROOT}/afanasy/python:{CGRU_ROOT}/lib/python:${{PYTHONPATH}}
+#         cmd_bash = r'''export PYTHONPATH={CGRU_ROOT}/afanasy/python:{CGRU_ROOT}/lib/python:${{PYTHONPATH}}
+# cd {CGRU_ROOT}
+# source {CGRU_ROOT}/setup.sh
+# python "{CGRU_ROOT}/afanasy/python/afjob.py" "{INP}" {START} {END} -by {STEP} -fpt 1 -seq 1 -pwd "{PWD}" -name "{NAME}"  -pause
+#         '''.format(
+#             CGRU_ROOT=CGRU_ROOT,
+#             INP=inp,
+#             START=self.job_param['job_start'],
+#             END=self.job_param['job_end'],
+#             STEP=self.job_param['job_step'],
+#             PWD=os.path.dirname(inp),
+#             NAME=os.path.basename(sceneName())
+#         ).replace('\\', '/')
+        cmd_bash = r'''
+export START_DIR="$( cd "$( dirname "${{BASH_SOURCE[0]}}" )" && pwd )"
+export PYTHONPATH={CGRU_ROOT}/afanasy/python:{CGRU_ROOT}/lib/python:${{PYTHONPATH}}
 cd {CGRU_ROOT}
 source {CGRU_ROOT}/setup.sh
-python "{CGRU_ROOT}/afanasy/python/afjob.py" "{INP}" {START} {END} -by {STEP} -fpt 1 -seq 1 -pwd "{PWD}" -name "{NAME}"  -pause
-        '''.format(
+python "{CGRU_ROOT}/afanasy/bin/afcmd" json send ${{START_DIR}}/job.json
+'''.format(
             CGRU_ROOT=CGRU_ROOT,
-            INP=inp,
-            START=self.job_param['job_start'],
-            END=self.job_param['job_end'],
-            STEP=self.job_param['job_step'],
-            PWD=os.path.dirname(inp),
-            NAME=os.path.basename(sceneName())
+            # INP=inp,
+            # START=self.job_param['job_start'],
+            # END=self.job_param['job_end'],
+            # STEP=self.job_param['job_step'],
+            # PWD=os.path.dirname(inp),
+            # NAME=os.path.basename(sceneName())
         ).replace('\\', '/')
+
         open(os.path.normpath(os.path.join(os.path.dirname(inp), 'render.sh')), 'w').write(cmd_bash)
+
 
 
 def replace_class():
     meArnoldRender.meArnoldRender = meArnoldRenderVersion
     print('CGRU Arnold updated')
+
+
+json_template = {
+    "job":
+        {
+            "name": "Render Ass",
+            "user_name": getpass.getuser(),
+            "host_name": socket.gethostname(),
+            "blocks": [
+                {
+                    "name": "Ass Render",
+                    "tasks_name": "frames @#@-@#@",
+                    "flags": 1,
+                    "service": "arnold",
+                    "parser": "arnold",
+                    "frame_first": 0,
+                    "frame_last": 0,
+                    "frames_per_task": 0,
+                    "frames_inc": 1,
+                    "command": r'kick -i "...\untitled_0001.ass" -v 2 -nstdin -dw -dp',
+                    "working_directory": "",
+                    # "files": ["folder/untitled_@####@.exr"]
+                }
+            ]
+        }
+}
